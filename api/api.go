98655133fe6a7632604ed70ddcda87cfde953ddf
Ryan S <rw.sarge@gmail.com>
2019-01-15 22:00:54 -0800

    NewUserAPI

--- api/api.go
+++ api/api.go
@@ -1,14 +1,45 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+	"encoding/json"
+	"bytes"
+)
+
 type (
 	Api interface {
-		GetGame() (Game, error)
+		NewUser(username, password string) BaseResponse
+	}
+
+	Server struct {
+		domain string
 	}
 )
-func GetGame() (Game, error) {
-	resp, err := http.Get("")
-	if err != nil {
 
+func (server Server) NewUser(username, password string) (BaseResponse, error) {
+	req := struct {
+		username string
+		password string
+	}{
+		username,
+		password,
 	}
+	var baseResponse BaseResponse
+	if err := call(server.domain + "/newuser", req, &baseResponse); err != nil {
+		return baseResponse, nil
+	} else {
+		return BaseResponse{}, err
+	}
+}
+
+func call(path string, body interface{}, response interface{}) error {
+	buffer := new(bytes.Buffer)
+	json.NewEncoder(buffer).Encode(body)
+	rawResponse, err := http.Post(path, "application/json", buffer)
+	if err != nil {
+		return err
+	}
+	decoder := json.NewDecoder(rawResponse.Body)
+	decoder.Decode(response)
+	return nil
 }
